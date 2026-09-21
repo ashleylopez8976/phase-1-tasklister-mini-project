9aca9ad73d753234599148ba3a3799fd7c5bd915
Ashley Lopez <ashleylopez8976@gmail.com>
2026-09-21 16:00:56 -0500

    Add task form submission and list display

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskDescription = event.target.elements["new-task-description"].value;
+
+    buildToDo(taskDescription);
+  });
+
+  function buildToDo(taskDescription) {
+    const taskItem = document.createElement("li");
+    taskItem.textContent = taskDescription;
+
+    const taskList = document.getElementById("tasks");
+    taskList.appendChild(taskItem);
+   }
 });
